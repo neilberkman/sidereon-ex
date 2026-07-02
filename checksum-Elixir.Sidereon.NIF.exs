4136627ab3e30b1ff2d6dfe0fc178ae2c75fa3f3
Neil Berkman <neil@xuku.com>
2026-07-02 07:17:27 -0700

    0.10.1: precompiled NIF checksums

--- checksum-Elixir.Sidereon.NIF.exs
+++ checksum-Elixir.Sidereon.NIF.exs
@@ -1,7 +1,7 @@
 %{
-  "libsidereon_nif-v0.10.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:2bb8194f69cb2b8f137909a24164b509ca545972ee9a63fcd659c7af88886656",
-  "libsidereon_nif-v0.10.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:75139051906f24ac1aad5d29ef4b5acb69f0fd86087d2c93db52fa27ad240ca0",
-  "libsidereon_nif-v0.10.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:fd490f5e8d1eb82d84ee984484576c6bf7df4f7edbe5734df4d0869f3f17110e",
-  "libsidereon_nif-v0.10.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:acaebcca1169d9a04554d4a9bacea6c5625b08d8db45ae87c4207fe476a94e63",
-  "sidereon_nif-v0.10.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:a5a047d74df3071d208017b00e86d67fd80acf50a9fa24c10fed3cc2f3d78157",
+  "libsidereon_nif-v0.10.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:9808daf68c699c6645b72ee2781cc3f84f71eff9403d4d8355cf12538907f6a3",
+  "libsidereon_nif-v0.10.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:a440c2aa41a15dacf67d6ed2edcb6f97cad89e20ca052eef72f80cabbaad3475",
+  "libsidereon_nif-v0.10.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:ee66c94bd89bfc44079da0282282c2cedf14674d6e7b566ed964b91d670b2e1a",
+  "libsidereon_nif-v0.10.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:7e8259974a40c005801c893715d6347bd0116b4eaf2338c0d7bb27972ceadab7",
+  "sidereon_nif-v0.10.1-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:fe39c26d05064cb6edcd719abc4d569b98e067b0f06ede7f0df8413eb28deb7a",
 }
